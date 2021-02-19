5027ef01dafe95f6b53bbaeca624d8baf8272ffd
Cody Alvarez <cody_alvarez19@outlook.com>
2021-02-19 00:37:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,12 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
+def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
+  if phrase.capitalize
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
