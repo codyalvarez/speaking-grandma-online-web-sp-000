3078a8415af5e39744c996ac3a734e0dbd0c9230
Cody Alvarez <cody_alvarez19@outlook.com>
2021-02-19 00:39:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase.capitalize
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
